c4ede79122ca5f3b9404a843be7459a6a90fc613
4lkaid <4lkaid@gmail.com>
2023-04-15 20:55:03 +0800

    feat(general): 优雅的终止服务
    1. ["macros", "rt-multi-thread"] -> ["macros", "rt-multi-thread", "signal"]
    2. ctrl+c 或 kill 强制终止服务将写入错误日志

--- src/config/general.rs
+++ src/config/general.rs
@@ -1,6 +1,7 @@
 use anyhow::Result;
 use serde::Deserialize;
 use std::{net::SocketAddr, str::FromStr};
+use tokio::signal;
 
 use crate::route;
 
@@ -16,7 +17,34 @@ pub async fn init(config: &General) -> Result<()> {
     let api = route::api::init();
     axum::Server::bind(&addr)
         .serve(api.into_make_service())
+        .with_graceful_shutdown(shutdown_signal())
         .await
         .unwrap();
     Ok(())
+}
+
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
+
+    tracing::error!("强制终止服务");
 }
