562be94f666158c6f34c33eaf7185e98401a05f0
4lkaid <4lkaid@gmail.com>
2023-04-15 15:38:07 +0800

    feat(logger): 支持调试模式
    1. 日志级别 TRACE > DEBUG > INFO > WARN > ERROR
    2. 默认最高日志级别为 INFO
    3. 开启调试模式后, 默认最高日志级别为 DEBUG
    4. INFO 级别及以下的日志将写入日志文件中, DEBUG 级别及以上的日志将输出到终端

--- src/config/logger.rs
+++ src/config/logger.rs
@@ -1,10 +1,15 @@
 use anyhow::Result;
 use serde::Deserialize;
 use tracing_appender::non_blocking::WorkerGuard;
-use tracing_subscriber::fmt::time::LocalTime;
+use tracing_subscriber::{
+    fmt::{time::LocalTime, writer::MakeWriterExt},
+    prelude::__tracing_subscriber_SubscriberExt,
+    util::SubscriberInitExt,
+};
 
 #[derive(Deserialize)]
 pub struct Logger {
+    pub debug: bool,
     pub directory: String,
     pub file_name_prefix: String,
 }
@@ -14,16 +19,33 @@ pub fn init(config: &Logger) -> Result<WorkerGuard> {
         "[year]-[month]-[day] [hour]:[minute]:[second]",
     )?);
 
+    let debug_layer = if config.debug {
+        let debug_layer = tracing_subscriber::fmt::layer()
+            .pretty()
+            .with_ansi(true)
+            .with_timer(timer.clone())
+            .with_writer(
+                std::io::stdout
+                    .with_min_level(tracing::Level::DEBUG)
+                    .with_max_level(tracing::Level::DEBUG),
+            );
+        Some(debug_layer)
+    } else {
+        None
+    };
+
     let file_appender =
         tracing_appender::rolling::daily(&config.directory, &config.file_name_prefix);
     let (non_blocking, worker_guard) = tracing_appender::non_blocking(file_appender);
-
-    tracing_subscriber::fmt()
-        // .pretty()
+    let info_layer = tracing_subscriber::fmt::layer()
         .with_ansi(false)
-        .with_max_level(tracing::Level::DEBUG)
         .with_timer(timer)
-        .with_writer(non_blocking)
+        .with_writer(non_blocking.with_max_level(tracing::Level::INFO));
+
+    tracing_subscriber::registry()
+        .with(debug_layer)
+        .with(info_layer)
         .init();
+
     Ok(worker_guard)
 }
